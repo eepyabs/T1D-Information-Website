07108fd4dd3e5b7bf59448addff630e3fd1b416e
Abbie Wallen <abbiechan101@gmail.com>
2026-08-06 09:22:16 -0500

    Fixed image issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 export const metadata: Metadata = {
     title: "T1D World Updates",
     description: "Recent Type 1 diabetes research and clinical trial updates.",
-    icons: { icon: "/t1d-ribbon.png", }
+    icons: { icon: "/t1d-ribbon.webp", }
 };
 
 export default function RootLayout({
