45d977995986a6c1b3d9af6f8c35b7beb980c921
Abbie Wallen <abbiechan101@gmail.com>
2026-08-06 09:14:32 -0500

    Tab image update

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import "./globals.css";
 export const metadata: Metadata = {
     title: "T1D World Updates",
     description: "Recent Type 1 diabetes research and clinical trial updates.",
+    icons: { icon: "/t1d-ribbon.png", }
 };
 
 export default function RootLayout({
